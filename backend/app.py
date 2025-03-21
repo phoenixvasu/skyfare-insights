da71b6ca6f2bcb5f7959e0694bbe7bfca11bda38
phoenixvasu <vasu21aug@gmail.com>
2025-03-22 01:47:53 +0530

    specified port for deployment

--- backend/app.py
+++ backend/app.py
@@ -134,4 +134,6 @@ def predict():
             return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
